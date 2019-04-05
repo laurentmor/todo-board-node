efc1e749841ea3d5b70d0cfafc812543400402e7
Laurent Morissette <laurent.morissette@gmail.com>
2019-04-04 21:51:58 -0400

    Can now generate user data Closes #18.

--- todo-api/index.js
+++ todo-api/index.js
@@ -20,8 +20,8 @@ mongoose.connect(DB, {useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 
 // BodyParser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(cookieParser());
 
 // Express Session
@@ -34,97 +34,28 @@ app.use(session({
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.get('/data', function (req, res) {
+    var jsf = require('json-schema-faker');
+    var schema = require('./datasets/user-schema');
+    var fs = require('fs');
+    jsf.option({
+        failOnInvalidTypes: false,
+        maxItems: 20000
+    });
+    var samples = [];
+    for (var i = 0; i < 20000; i++) {
+        samples[i] = jsf.generate(schema);
+    }
+    fs.writeFile('./datasets/userList.json', JSON.stringify(samples), function (err) {
+        if (err) throw err;
+        res.send(samples);
+    })
+
+
+});
 app.use('/users', UserRouter);
 passportConfig.setup();
 
-/*
 
-var FacebookStrategy = require('passport-facebook').Strategy;
-passport.use(new FacebookStrategy({
-    clientID: "145658462755484",
-    clientSecret: "ed6555b4e3c8b42764659a2b9c861825",
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(profile)
-    User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-      if (err) return done(err);
-      if (user) return done(null, user);
-      else {
-        // if there is no user found with that facebook id, create them
-        var newUser = new User();
-
-        // set all of the facebook information in our user model
-        newUser.facebook.id = profile.id;
-        newUser.facebook.token = accessToken;
-        newUser.facebook.name  = profile.displayName;
-        if (typeof profile.emails != 'undefined' && profile.emails.length > 0)
-          newUser.facebook.email = profile.emails[0].value;
-
-        // save our user to the database
-        newUser.save(function(err) {
-            if (err) throw err;
-            return done(null, newUser);
-        });
-      }
-    });
-  }
-));
-
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    console.log(req.user)
-    res.redirect('/');
-  }
-);
-
-
-const CanCan = require('cancan');
-const cancan = new CanCan();
-const {allow, can} = cancan;
-
-allow(User, ['view', 'edit'], Post, (user, post) => post.ownerId === user.id);
-
-app.get("/posts/:id", function(req, res){
-  Post.find({}, function(err, all) {
-    console.log(all)
-  })
-  Post.findById(req.params.id, function(err, post){
-    if (can(req.user, 'view', post)) {
-      res.send(JSON.stringify(post))
-    }else{
-      res.status(403).send("{errors: \"Unauthorized to view this post\"}").end()
-    }
-  });
-});
-
-
-
-// Just for testing as we don't have an interface to add posts
-app.get("/createDummies", function(req, res){
-  var newPost1 = new Post({
-    title: "Dimmie1",
-    ownerId: req.user.id,
-    body: "Dummie body for post"
-  });
-  newPost1.save()
-
-  var newPost2 = new Post({
-    title: "Dimmie2",
-    ownerId: req.user.id,
-    body: "Dummie body for post"
-  });
-  newPost2.save()
-
-  res.send(JSON.stringify([newPost1, newPost2]))
-})
-*/
-
-// noinspection BadExpressionStatementJS
-//app.listen(3000, () => console.log('Example app listening on port 3000!'))
 app.listen(port, console.log('Auth API listening on port ' + port));
