ea1955aa8e6a9f40baf296e6cbee75c2c3866022
Laurent Morissette <Laurent.morissette@gmail.com>
2020-09-15 16:39:04 -0400

    done apply limiting

--- todo-api/src/index.js
+++ todo-api/src/index.js
@@ -11,14 +11,23 @@ import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import mongoose from 'mongoose';
-
+import rateLimit from 'express-rate-limit';
 
 import logger from "./config/logger-config";
 import router from './routes/todos';
 
 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
 
 const app = express();
+app.use(limiter);
 app.get("/", function (req, res) {
 
     const msg = "Hello World!";
